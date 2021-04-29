980d5a4e2b1dde64f8383d65d2ef8ea231e8ee86
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2021-04-29 14:51:02 -0500

    Conexión a base de datos
    Falla en la clave de mi DB

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,12 +5,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace MVP_Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string correoUsuario { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -21,5 +25,22 @@ namespace MVP_Web.Pages
         {
 
         }
+
+        public void OnPost()
+        {
+            string connectionString = "Server=127.0.0.1;Port=3306;Database=TerniumDB;Uid=root;password=password;";
+            MySqlConnection conexion = new MySqlConnection(connectionString);
+            conexion.Open();
+
+            var sql = "INSERT INTO Bitacora(Usuario_Id, Fecha) VALUES(@id, @fecha)";
+            using var cmd = new MySqlCommand(sql, conexion);
+
+            cmd.Parameters.AddWithValue("@id", correoUsuario);
+            cmd.Parameters.AddWithValue("@fecha", DateTime.Now.Date);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+        }
+
     }
 }
