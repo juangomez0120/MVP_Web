9a8d3c84a46860c2881646c5a1bdf97b55fd04f0
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2021-04-30 14:04:56 -0500

    Update DatosUsuario.cshtml.cs

--- Pages/DatosUsuario.cshtml.cs
+++ Pages/DatosUsuario.cshtml.cs
@@ -31,7 +31,7 @@ namespace MVP_Web.Pages
 
             var cmd = new MySqlCommand(query1, conexion);
 
-            cmd.Parameters.Add("@id",MySqlDbType.VarChar).Value = "juanmagomez"; // CAMBIAR!!!!!!!
+            cmd.Parameters.Add("@id",MySqlDbType.VarChar).Value = "cristobale2000"; // CAMBIAR!!!!!!!
             cmd.Parameters.Add("@nombre", MySqlDbType.VarChar).Value = PrimeraLetraMayuscula(NombreUsuario);
             cmd.Parameters.Add("@apellido", MySqlDbType.VarChar).Value = PrimeraLetraMayuscula(ApellidoUsuario);
             cmd.ExecuteNonQuery();
