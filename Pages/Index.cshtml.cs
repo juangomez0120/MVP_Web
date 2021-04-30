4c206b7e6a902502d3484b4e257f839eb48917f8
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2021-04-30 03:08:50 -0500

    Queries para Usuario y Bitacora
    La aplicación web actualiza las tablas de Usuario y Bitácora en la bd. Falta la referencia del UsernameUsuario (intex.cshtml.cs) en el archivo DatosUsuario.cshtml.cs y verificar el método de PrimeraLetraMayuscula.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,10 +11,42 @@ namespace MVP_Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
         [BindProperty]
-        public string correoUsuario { get; set; }
+        public string UsernameUsuario { get; set; }
+
+        public IActionResult OnPost()
+        {
+            string connectionString = "Server=127.0.0.1;Port=3306;Database=DB_Gran_Escape;Uid=root;password=root;";
+            MySqlConnection conexion = new MySqlConnection(connectionString);
+            conexion.Open();
+
+            var query1 = "SELECT * FROM Usuario WHERE Id = @id;";
+
+            var cmd = new MySqlCommand(query1, conexion);
+            cmd.Parameters.Add("@id", MySqlDbType.VarChar).Value = UsernameUsuario;
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            
+
+            if (reader.Read())
+            {
+                reader.Close();
+                cmd.CommandText = "INSERT INTO Bitacora(Usuario_Id, Fecha) VALUES(@id, @fecha)";
+                cmd.Parameters.Add("@fecha", MySqlDbType.DateTime).Value = DateTime.Now;
+                cmd.ExecuteNonQuery();
+
+                return RedirectToPage("menu");
+            }
+            else
+            {
+                reader.Close();
+                return RedirectToPage("DatosUsuario");
+            }
+            
+            
+        }
+
+        private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -24,22 +56,6 @@ namespace MVP_Web.Pages
         public void OnGet()
         {
 
-        }
-
-        public void OnPost()
-        {
-            string connectionString = "Server=127.0.0.1;Port=3306;Database=TerniumDB;Uid=root;password=password;";
-            MySqlConnection conexion = new MySqlConnection(connectionString);
-            conexion.Open();
-
-            var sql = "INSERT INTO Bitacora(Usuario_Id, Fecha) VALUES(@id, @fecha)";
-            using var cmd = new MySqlCommand(sql, conexion);
-
-            cmd.Parameters.AddWithValue("@id", correoUsuario);
-            cmd.Parameters.AddWithValue("@fecha", DateTime.Now.Date);
-            cmd.Prepare();
-
-            cmd.ExecuteNonQuery();
         }
 
     }
