86b06b8437f7aa3affdcf49bea2181b9ae2bfcb7
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2021-05-12 15:06:48 -0500

    Login
    Servicios de Ternium REST y variables de sesión. Todavía falta verificar si podemos crear variables de sesión del tipo de dato "Login". Código de base de datos en Index.cshtml.cs comentado.

---
+++ Model/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MVP_Web.Model
+{
+    public class Login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string key { get; set; }
+        public string id { get; set; }
+    }
+}
