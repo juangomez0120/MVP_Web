365d9cbeca45ab2de47c0fb1ecfec5d320bae66a
Cristóbal Escamilla <cristobalescamilla2000@gmail.com>
2021-05-13 13:09:13 -0500

    Login Terminado

--- Pages/Perfil.cshtml.cs
+++ Pages/Perfil.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MVP_Web.Model;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace Sprint3.Pages
 {
     public class PerfilModel : PageModel
     {
+        public Login Usuario { get; set; }
+
         public void OnGet()
         {
+            string value = HttpContext.Session.GetString("SessionUsuario");
+
+            if (value != null)
+            {
+                Usuario = JsonConvert.DeserializeObject<Login>(value);
+            }
         }
     }
 }
